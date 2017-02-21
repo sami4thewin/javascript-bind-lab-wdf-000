cd3da4e5222111cde69cbed9144152b89e37eba7
sami4thewin <samiomarmohammed@gmail.com>
2017-02-21 17:14:29 -0500

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much.";
+
+
+
+const digitalClock = {
+  time: Math.round(Date.now()/1000),
+
+  startTicking(){
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+function censor(word, string) {
+  var reg = new RegExp(word, "g");
+  return string.replace(reg, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');
